4a80bb99721ac8b3671fe66ec755e6d1afe74790
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2025-12-06 22:55:02 +0530

    update config

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,11 +1,13 @@
-// config/passport.js
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/User');
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../models/userModal";
 
+// Serialize user id into session
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
+
+// Deserialize user from session
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id).lean();
@@ -15,43 +17,60 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL,
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    // profile contains e.g. profile.id, profile.emails[0].value, profile.displayName
-    const email = profile.emails && profile.emails[0] && profile.emails[0].value;
-    if (!email) return done(new Error('No email from Google'));
+// Google OAuth Strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email =
+          profile.emails &&
+          profile.emails[0] &&
+          profile.emails[0].value;
 
-    // Try to find by googleId first
-    let user = await User.findOne({ googleId: profile.id });
+        if (!email) return done(new Error("No email from Google"));
 
-    if (!user) {
-      // If there's already a user with same email (registered via email/password),
-      // decide whether to link accounts or reject. Here we link accounts.
-      user = await User.findOne({ email });
+        // Try to find user by googleId
+        let user = await User.findOne({ googleId: profile.id });
 
-      if (user) {
-        // Link Google to existing account
-        user.googleId = profile.id;
-        user.picture = profile.photos && profile.photos[0] && profile.photos[0].value;
-        await user.save();
-      } else {
-        // Create new user
-        user = await User.create({
-          username: profile.displayName || email.split('@')[0],
-          email,
-          googleId: profile.id,
-          picture: profile.photos && profile.photos[0] && profile.photos[0].value,
-          // password_hash left undefined for OAuth users
-        });
+        if (!user) {
+          // Try linking to existing account by email
+          user = await User.findOne({ email });
+
+          if (user) {
+            // Link Google account
+            user.googleId = profile.id;
+            user.picture =
+              profile.photos &&
+              profile.photos[0] &&
+              profile.photos[0].value;
+
+            await user.save();
+          } else {
+            // Create new OAuth user
+            user = await User.create({
+              username: profile.displayName || email.split("@")[0],
+              email,
+              googleId: profile.id,
+              picture:
+                profile.photos &&
+                profile.photos[0] &&
+                profile.photos[0].value,
+              // password_hash remains undefined
+            });
+          }
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
       }
     }
+  )
+);
 
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-}));
+export default passport;
