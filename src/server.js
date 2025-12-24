d4bd924660ee49d36cdbc332c85705e02fe3eab5
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2025-12-24 15:09:59 +0530

    update cors

--- src/server.js
+++ src/server.js
@@ -11,12 +11,12 @@ import cors from 'cors';
 
 const app = express();
 
-// const corsOptions = {
-//   origin: 'http://localhost:5173',
-//   credentials: true,
-// };
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: '100kb' }));
 
